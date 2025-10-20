4b068182767c52162cb3987f4d7e462f2f90dd55
Emanuel Jose <emanueljose0522@hotmail.com>
2025-10-20 11:34:36 -0400

    "added IUserservice and Userservice"

---
+++ ManagementDashboard/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using ManagementDashboard.Models;
+
+namespace ManagementDashboard.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByNameAsync(string name);
+        Task<User> AddUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<User> DeleteUserAsync(int id);
+    }
+}
---
+++ ManagementDashboard/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using ManagementDashboard.Models;
+
+namespace ManagementDashboard.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserService _userService;
+
+        public UserService(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<User> AddUserAsync(User user)
+        {
+            var existingUser = _userService.GetUserByIdAsync(user.Id).Result;
+            if (existingUser != null)
+            {
+                throw new InvalidOperationException("User with the same ID already exists.");
+            }
+
+            //create user
+            return await _userService.AddUserAsync(user);
+
+        }
+
+        public Task<User> DeleteUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
