eb4e734fe0d0a7714322be59fe19a8ff8f359233
emanul <emnaueljose0522@gmail.com>
2025-10-30 15:59:11 -0400

    finished the create user functionality with implenentation. Next is the Delete functionality

--- ManagementDashboard/Controllers/UserController.cs
+++ ManagementDashboard/Controllers/UserController.cs
@@ -19,9 +19,31 @@ namespace ManagementDashboard.Controllers
             return View(users);
         }
 
+
+        //GET: User/Create
         public IActionResult Create()
         {
             return View();
+        }
+
+        //Post: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Models.User user)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {   
+                    await _userService.AddUserAsync(user);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (System.Exception)
+                {
+                    ModelState.AddModelError(string.Empty, "An error occurred while creating the user.");
+                }
+            }
+            return View(user);
         }
     }
 }
